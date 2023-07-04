b6d891623120842296ded53e3e37d47d76fc5375
migena <migena.allushi1@gmail.com>
2023-07-04 17:57:27 +0200

    created playRound function

--- javascript.js
+++ javascript.js
@@ -4,12 +4,40 @@ function getComputerChoice(){
     let choice = choices[(Math.floor(Math.random() * choices.length))];
     return choice;
 }
-console.log(getComputerChoice())
 
-//create a functiion to get players input
+//create a function to get players input and then convert the answer to lowercase
 function getPlayerChoice(){
     let playerChoice = prompt("What is your choice: rock, paper or scissors?");
     let answer = playerChoice.toLowerCase();
     return answer;
 }
-console.log(getPlayerChoice())
+
+//create a function that plays 1 round of the game with 2 parameters as computers selection and players selection 
+//and than returns a winner
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
+        return("Same choice. No points given. Play again.");
+    }
+    else if ((playerSelection === "rock") & (computerSelection === "paper")){
+        return("You lose! Paper beats Rock.");
+    }
+    else if ((playerSelection === "rock") & (computerSelection === "scissors")){
+        return("You win! Rock beats Scissors.");
+    }
+    else if ((playerSelection === "paper") & (computerSelection === "rock")){
+        return("You win! Paper beats Rock.");
+    }
+    else if ((playerSelection === "paper") & (computerSelection === "scissors")){
+        return("You lose! Scissors beat Paper.");
+    }
+    else if ((playerSelection === "scissors") & (computerSelection === "paper")){
+        return("You win! Scissors beat Paper.");
+    }
+    else if ((playerSelection === "scissors") & (computerSelection === "rock")){
+        return("You lose! Rock beats Scissors.");
+    }
+}
+
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
+console.log(playRound(playerSelection, computerSelection));
