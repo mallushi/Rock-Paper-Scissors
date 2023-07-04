0ca7068fb0973caba73e27a1d9054f62b743ad10
migena <migena.allushi1@gmail.com>
2023-07-04 15:26:37 +0200

    created getPlayersChoice function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,15 @@
+//create a function to get a random choice from computer
 function getComputerChoice(){
     let choices = ['rock', 'paper', 'scissors'];
     let choice = choices[(Math.floor(Math.random() * choices.length))];
     return choice;
 }
 console.log(getComputerChoice())
+
+//create a functiion to get players input
+function getPlayerChoice(){
+    let playerChoice = prompt("What is your choice: rock, paper or scissors?");
+    let answer = playerChoice.toLowerCase();
+    return answer;
+}
+console.log(getPlayerChoice())
